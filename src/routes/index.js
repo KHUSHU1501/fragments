860187e9887cd4bb68f785588b1d98360a77a872
khushu <khushu1501@gmail.com>
2023-03-30 12:13:13 -0400

    Updated health check route to add host name

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,11 +7,10 @@ const { version, author } = require('../../package.json');
 
 // a router that we can use to mount our API
 const router = express.Router();
-
 // Our authentication middleware
 const { authenticate } = require('../authorization/index');
-
 const response = require('../response');
+const { hostname } = require('os');
 
 /**
  * Expose all of our API routes on /v1/* to include an API version.
@@ -23,14 +22,14 @@ router.use(`/v1`, authenticate(), require('./api'));
  * we'll respond with a 200 OK.  If not, the server isn't healthy.
  */
 router.get('/', (req, res) => {
-  // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
-  // Send a 200 'OK' response
   res.status(200).json(
     response.createSuccessResponse({
-      author,
+      author: author,
       githubUrl: 'https://github.com/KHUSHU1501/fragments',
       version,
+      // Include the hostname in the response
+      hostname: hostname(),
     })
   );
 });
